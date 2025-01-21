5865bc76cbb66e023ec821de8d6015813d9cef48
naiknetra <naiknetra5@gmail.com>
2025-01-21 09:59:19 -0500

    added ts files

---
+++ src/models/User.ts
@@ -0,0 +1,47 @@
+
+import {Schema , model, type Document} from "mongoose"
+interface IUser extends Document {
+    usename: string,
+    email: string,
+    thoughts: Schema.Types.ObjectId[],
+    friends: Schema.Types.ObjectId[]
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+
+
+
